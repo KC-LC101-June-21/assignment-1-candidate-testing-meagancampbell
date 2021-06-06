efb4c102326f3fe4d84f412bf2cd4968fca669c7
Meagan Campbell <>
2021-06-06 21:34:22 +0000

    mc

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-const quiz = require('./candidate-testing');
+const input = require('readline-sync');
 
-quiz.runProgram();
+// TODO 2: modify your quiz app to ask 5 questions //
+
+// TODO 1.1a: Define candidateName // 
+let candidateName;
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let question;
+let correctAnswer;
+let candidateAnswer;
+let questions;
+let correctAnswers;
+let candidateAnswers;
+
+let info = input.question("What is your name?");
+function askForName() {
+  // TODO 1.1b: Ask for candidate's name //
+console.log("What is your name?");
+}
+
+let name = input.question("What is 44 / 2 ?");
+
+function askQuestion() {
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+
+
+}
+
+function gradeQuiz(candidateAnswers) {
+
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
+
+  let grade;
+  
+
+  return grade;
+}
+
+function runProgram() {
+  askForName();
+  // TODO 1.1c: Ask for candidate's name //
+  
+  askQuestion();
+  gradeQuiz(this.candidateAnswers);
+}
+
+// Don't write any code below this line //
+// And don't change these or your program will not run as expected //
+module.exports = {
+  candidateName: candidateName,
+  question: question,
+  correctAnswer: correctAnswer,
+  candidateAnswer: candidateAnswer,
+  questions: questions,
+  correctAnswers: correctAnswers,
+  candidateAnswers: candidateAnswers,
+  gradeQuiz: gradeQuiz,
+  runProgram: runProgram
+};
