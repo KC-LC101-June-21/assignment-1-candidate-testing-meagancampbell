b198725f8bfaa5dedbeb15414cac597dc5860519
Meagan Campbell <>
2021-06-07 19:53:05 +0000

    MC

--- index.js
+++ index.js
@@ -4,70 +4,79 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
-let correctAnswer = 0;
-let incorrectAnswer = 0;
-let question = "none";
-let input = "none";
-let answer = "none";
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let score = 0;
+let question;
+let correctAnswer;
 let candidateAnswer;
-let questions;
+let incorrectAnswer;
 let correctAnswers;
 let candidateAnswers;
 
-let info = input.question("What is your name?");
-function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-console.log("Hello + name + let's begin the quiz");
-}
 
-question = "Are you a U.S Citizen?";
-answer = "yes"
+candidateName = prompt("What is your name?");
 
-question = "Do you have a masters degree in a STEM field, including engineering, biological science, physical science, computer science or mathematics, from an accredited institution.";
-answer = "yes"
+let questionOne = prompt("Are you a U.S. Citizen?");
 
-question = "Do you have at least two years of related professional experience obtained after degree completion or at least 1,000 hours pilot-in-command time on a jet aircraft?";
-answer = "yes"
+if (candidateAnswer === "Yes") {
+    console.log("Correct");
 
-question = "Are you able to pass the NASA long-duration flight astronaut physical?";
-answer = "yes"
-
-question = "And finally, what is NASA's motto?";
-answer = "For the benefit of all."
-
-
-function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  } else if (candidateAnswer === "No") {
+        console.log("Incorrect");
 
 }
 
-if (newVal === "") {
-    // user pressed OK, but the input field was empty
-} else if (newVal) {
-    // user typed something and hit OK
-} else {
 
-function gradeQuiz(candidateAnswers) {
+let questionTwo = prompt("Do you have a masters degree in a STEM field, including engineering, biological science, physical science, computer science or mathematics, from an accredited institution?");
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+if (candidateAnswer === "Yes") {
+    console.log("Correct");
 
+  } else if (candidateAnswer === "No") {
+        console.log("Incorrect");
 
-  let grade;
-  
-
-  return grade;
 }
 
-function runProgram() {
-  askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  
-  askQuestion();
-  gradeQuiz(this.candidateAnswers);
+let questionTwo = prompt("Do you have a masters degree in a STEM field, including engineering, biological science, physical science, computer science or mathematics, from an accredited institution?");
+
+if (candidateAnswer === "Yes") {
+    console.log("Correct");
+
+  } else if (candidateAnswer === "No") {
+        console.log("Incorrect");
+
 }
+
+let questionThree = prompt("Do you have at least two years of related professional experience obtained after degree completion or at least 1,000 hours pilot-in-command time on a jet aircraft?")
+if (candidateAnswer === "Yes") {
+    console.log("Correct");
+
+  } else if (candidateAnswer === "No") {
+        console.log("Incorrect");
+
+}
+
+let questionFour = prompt("Are you able to pass the NASA long-duration flight astronaut physical?")
+if (candidateAnswer === "Yes") {
+    console.log("Correct");
+
+  } else if (candidateAnswer === "No") {
+        console.log("Incorrect");
+
+}
+
+let questionFive = prompt("And finally, what is NASA's motto?")
+if (candidateAnswer === "For the benefit of all") {
+    console.log("Correct");
+
+  } else if (candidateAnswer === "") {
+        console.log("Incorrect");
+
+}
+    
+    
+//this sucks!!!!!
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
