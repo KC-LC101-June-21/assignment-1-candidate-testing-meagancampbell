295d11caa7e764783112a209a37732ee02e81518
Meagan Campbell <>
2021-06-07 21:26:24 +0000

    FML

--- index.js
+++ index.js
@@ -19,63 +19,62 @@ candidateName = prompt("What is your name?");
 
 let questionOne = prompt("Are you a U.S. Citizen?");
 
-if (candidateAnswer === "Yes") {
-    console.log("Correct");
+if (questionOne.candidateAnswer === "Yes"); {
+  console.log("Correct");
 
-  } else if (candidateAnswer === "No") {
-        console.log("Incorrect");
-
-}
-
-
-let questionTwo = prompt("Do you have a masters degree in a STEM field, including engineering, biological science, physical science, computer science or mathematics, from an accredited institution?");
-
-if (candidateAnswer === "Yes") {
-    console.log("Correct");
-
-  } else if (candidateAnswer === "No") {
-        console.log("Incorrect");
+} else if (candidateAnswer === "No") {
+  console.log("Incorrect");
 
 }
 
 let questionTwo = prompt("Do you have a masters degree in a STEM field, including engineering, biological science, physical science, computer science or mathematics, from an accredited institution?");
 
-if (candidateAnswer === "Yes") {
-    console.log("Correct");
+if (questionTwo.candidateAnswer === "Yes") {
+  console.log("Correct");
 
-  } else if (candidateAnswer === "No") {
-        console.log("Incorrect");
-
-}
-
-let questionThree = prompt("Do you have at least two years of related professional experience obtained after degree completion or at least 1,000 hours pilot-in-command time on a jet aircraft?")
-if (candidateAnswer === "Yes") {
-    console.log("Correct");
-
-  } else if (candidateAnswer === "No") {
-        console.log("Incorrect");
+} else if (candidateAnswer === "No") {
+  console.log("Incorrect");
 
 }
 
-let questionFour = prompt("Are you able to pass the NASA long-duration flight astronaut physical?")
-if (candidateAnswer === "Yes") {
-    console.log("Correct");
+let questionThree = prompt("Do you have a masters degree in a STEM field, including engineering, biological science, physical science, computer science or mathematics, from an accredited institution?");
 
-  } else if (candidateAnswer === "No") {
-        console.log("Incorrect");
+if (questionThree.candidateAnswer === "Yes") {
+  console.log("Correct");
+
+} else if (candidateAnswer === "No") {
+  console.log("Incorrect");
+
+}
+
+let questionFour = prompt("Do you have at least two years of related professional experience obtained after degree completion or at least 1,000 hours pilot-in-command time on a jet aircraft?")
+if (questionFour.candidateAnswer === "Yes") {
+  console.log("Correct");
+
+} else if (candidateAnswer === "No") {
+  console.log("Incorrect");
+
+}
+
+let questionFive = prompt("Are you able to pass the NASA long-duration flight astronaut physical?")
+if (questionFive.candidateAnswer === "Yes") {
+  console.log("Correct");
+
+} else if (candidateAnswer === "No") {
+  console.log("Incorrect");
 
 }
 
 let questionFive = prompt("And finally, what is NASA's motto?")
-if (candidateAnswer === "For the benefit of all") {
-    console.log("Correct");
+if (questionFive.candidateAnswercandidateAnswer === "For the benefit of all") {
+  console.log("Correct");
 
-  } else if (candidateAnswer === "") {
-        console.log("Incorrect");
+} else if (candidateAnswer === "") {
+  console.log("Incorrect");
 
 }
-    
-    
+
+
 //this sucks!!!!!
 
 // Don't write any code below this line //
