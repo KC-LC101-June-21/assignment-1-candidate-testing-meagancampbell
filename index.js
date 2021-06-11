d2df4602d8c3f62ebafc648a908fab791a837b09
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-11 00:22:33 +0000

    MC

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ let questions;
 let correctAnswers;
 let candidateAnswers;
 
-function total (){
-sum = a + b + c + d;
-console.log = sum;
 
-}
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 
@@ -36,10 +32,46 @@ if (value == questions.answer) {
 }
 
 
+
+
 candidateName = prompt("What is your name?");
 
+Who was the first American woman in space?	"Sally Ride"
+True or false: 5 kilometer == 5000 meters?	"true"
+(5 + 3)/2 * 10 = ?	"40"
+Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?	"Trajectory"
+What is the minimum crew size for the ISS?	"3"
 
+let questions = [{ 
+        question: 'Who was the first American woman in space?',
+        answer: 'Sally Ride',
+        affirm: 'Yay! You got it right!',
+        rebuttal: 'Nope, you are definitely happy.'
+    },
+    {
+        question: 'True or false: 5 kilometer == 5000 meters?',
+        answer: 'True',
+        affirm: 'Good job!',
+        rebuttal: 'Not right.'
+    },
+        {
+        question:'(5 + 3)/2 * 10 = ?',
+        answer: '40',
+        affirm: 'Good job!',
+        rebuttal: 'Not right.'
+    }];
 
+for (var i = 0, l = questions.length; i < l; i++) {
+    answer = prompt(questions[i].question);
+
+    // I do not support manipulating a loop counter mid-loop, but it's for an example.
+    if (answer !== questions[i].answer) {
+        alert(questions[i].rebuttal);
+        i--;
+    } else {
+        alert(questions[i].affirm);
+    }
+}
 
 
 
