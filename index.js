5f89df64005d909adcce731110ac41bb1e3c063a
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-11 20:05:29 +0000

    idk

--- index.js
+++ index.js
@@ -1,39 +1,135 @@
 const input = require('readline-sync');
 
+// TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
+
 let correctAnswer;
 let candidateAnswer;
-let questions;
+
 let correctAnswers;
 let candidateAnswers;
-let gradeQuiz;
 
 
 console.log("What is your name?");
 
-// TODO 2: modify your quiz app to ask 5 questions //
+// select all elements
+const start = document.write("start");
+const quiz = document.write("quiz");
+const question = document.write("question");
+
+const choiceA = document.write("A");
+const choiceB = document.write("B");
+const choiceC = document.write("C");
+const counter = document.write("counter");
 
 
 
+// create questions
+let questions = [
+    {
+        question : "What does HTML stand for?",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
+    }  
+];
 
-//this sucks!!!!!
+// create some variables
 
-// Don't write any code below this line //
-// And don't change these or your program will not run as expected //
-module.exports = {
-  candidateName: candidateName,
-  question: question,
-  correctAnswer: correctAnswer,
-  candidateAnswer: candidateAnswer,
-  questions: questions,
-  correctAnswers: correctAnswers,
-  candidateAnswers: candidateAnswers,
-  gradeQuiz: gradeQuiz,
-  runProgram: runProgram
-};
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+
+
+let score = 0;
+
+// render a question
+function renderQuestion(){
+    let q = questions[runningQuestion];
+    
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
+//start.addEventListener("click",startQuiz);
+
+// start quiz
+function startQuiz(){
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+    renderCounter();
+    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+}
+
+// render progress
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    }
+}
+
+// counter render
+
+function renderCounter(){
+    if(count <= questionTime){
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++
+    }else{
+        count = 0;
+        // change progress color to red
+        answerIsWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+}
+
+// checkAnwer
+
+function checkAnswer(answer){
+    if( answer == questions[runningQuestion].correct){
+        // answer is correct
+        score++;
+        // change progress color to green
+        answerIsCorrect();
+    }else{
+        // answer is wrong
+        // change progress color to red
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
+    }
+}
+
+// answer is correct
+function answerIsCorrect(){
+    document.write(runningQuestion);
+}
+
+// answer is Wrong
+function answerIsWrong(){
+    document.write(runningQuestion);
+}
 
