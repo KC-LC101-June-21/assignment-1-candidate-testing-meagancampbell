b32b2027466eccd4d5c9cb7380c906aefaea4864
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-12 03:05:13 +0000

    1 working question, still need to figure out user input answer

--- index.js
+++ index.js
@@ -4,18 +4,88 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 
+candidateName = input.question('Enter your name: ');
+console.log('Hello Astronaut Candidate ' + candidateName);
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
 let correctAnswer;
 let candidateAnswer;
+let answersArray = ["a", "True", "40", "Trajectory", "3" ] ;
+let candidateAnswers= [];
+//const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+let score = 0;
 
-let correctAnswers;
-let candidateAnswers;
+// var doAction = function () {
+//     var input = document.getElementById('numberinput');
+//     var times = parseint(input.value);
+
+//     for(var i = 0; i < times; i++) {
+//       //do whatever you need to do
+//     }
+// }
 
 
-let candidateName = input.question("Enter your name: ");
-console.log("Your name is " + candidateName);
+
+let correct = "AA";
+candidateAnswer = "";
+
+
+console.log("1. Who was the first American woman in space?");
+console.log('   a: Sally Ride' + '\n', '  b: Christa McAuliffe'+ '\n', '  c: Valentina Tereshkova'+ '\n'); 
+
+function questionAnswer(candidateAnswer = True){
+  if (candidateAnswer == True){
+    console.log ("Correct");
+  } else {
+    console.log ("Wrong");
+  }
+}
+
+
+// quiz = get("quiz");
+//   if(pos >= questions.length){
+//     test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+//     get("quiz_status").innerHTML = "Quiz completed";
+//     // resets the variable to allow users to restart the test
+//     pos = 0;
+//     correct = 0;
+//     // stops rest of renderQuestion function running when test is completed return false;
+// }
+
+// get("quiz_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+  
+//   question = questions[pos].question;
+//   chA = questions[pos].a;
+//   chB = questions[pos].b;
+//   chC = questions[pos].c;
+
+
+
+// function checkAnswer(){
+//   // use getElementsByName because we have an array which it will loop through
+//   choices = document.getElementsByName("choices");
+//   for(let i=0; i<choices.length; i++){
+//     if(choices[i].checked){
+//       choice = choices[i].value;
+//     }
+//   }
+//   // checks if answer matches the correct choice
+//   if(choice == questions[pos].answer){
+//     //each time there is a correct answer this value increases
+//     correct++;
+//   }
+//   // changes position of which character user is on
+//   pos++;
+//   // then the renderQuestion function runs again to go to next question
+//   renderQuestion();
+// }
+
+
+
+
 
 // // select all elements
 // const start = document.write("start");
@@ -26,111 +96,4 @@ console.log("Your name is " + candidateName);
 // const choiceB = document.write("B");
 // const choiceC = document.write("C");
 // const counter = document.write("counter");
-
-
-
-// create questions
-let questions = [
-    {
-        question : "What does HTML stand for?",
-        choiceA : "Correct",
-        choiceB : "Wrong",
-        choiceC : "Wrong",
-        correct : "A"
-    }  
-];
-
-// create some variables
-
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-let count = 0;
-
-
-let score = 0;
-
-// render a question
-function renderQuestion(){
-    let q = questions[runningQuestion];
-    
-    question.innerHTML = "<p>"+ q.question +"</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-}
-
-//start.addEventListener("click",startQuiz);
-
-// start quiz
-function startQuiz(){
-    start.style.display = "none";
-    renderQuestion();
-    quiz.style.display = "block";
-    renderProgress();
-    renderCounter();
-    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
-}
-
-// render progress
-function renderProgress(){
-    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
-        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
-    }
-}
-
-// counter render
-
-function renderCounter(){
-    if(count <= questionTime){
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count++
-    }else{
-        count = 0;
-        // change progress color to red
-        answerIsWrong();
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
-            renderQuestion();
-        }else{
-            // end the quiz and show the score
-            clearInterval(TIMER);
-            scoreRender();
-        }
-    }
-}
-
-// checkAnwer
-
-function checkAnswer(answer){
-    if( answer == questions[runningQuestion].correct){
-        // answer is correct
-        score++;
-        // change progress color to green
-        answerIsCorrect();
-    }else{
-        // answer is wrong
-        // change progress color to red
-        answerIsWrong();
-    }
-    count = 0;
-    if(runningQuestion < lastQuestion){
-        runningQuestion++;
-        renderQuestion();
-    }else{
-        // end the quiz and show the score
-        clearInterval(TIMER);
-        scoreRender();
-    }
-}
-
-// answer is correct
-function answerIsCorrect(){
-    document.write(runningQuestion);
-}
-
-// answer is Wrong
-function answerIsWrong(){
-    document.write(runningQuestion);
-}
 
