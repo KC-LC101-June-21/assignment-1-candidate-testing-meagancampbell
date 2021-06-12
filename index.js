4d6a870ec6b6486dc7cef529e0f3d1ba68f9261c
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-12 23:54:11 +0000

    MC

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ function runProgram() {
   gradeQuiz(this.candidateAnswers);
 }
 
+
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
