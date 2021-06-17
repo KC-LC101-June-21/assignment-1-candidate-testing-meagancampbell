ed748ed3decd3f875dec0b467d367dd6d434ad71
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-17 17:57:53 +0000

    questions and return of "your answer and correct answer are correct

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,22 +4,35 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
+let question1 = ['1) Who was the first American woman in space?' + '\n'];
+let question2 = ['2) True or false: 5 kilometer = 5000 meters?' + '\n'];
+let question3 = ['3) (5 + 3)/2 * 10 =' + '\n'];
+let question4 = ['4) Given the array [8,' + ' Orbit' + ' ,' + 'Trajectory' + ' ,' + '45] what entry is at index 2?' + '\n'];
+let question5 = ['5) What is the minimum crew size for the ISS?' + '\n'];
 let correctAnswer;
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [question1, question2, question3, question4, question5];
+let correctAnswers = ['sally ride', 'true', '40', 'trajectory', '3'];
+let candidateAnswers = [];
+let i = 0;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  let candidateName = input.question('Candidate Name: ');
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+ for (let i = 0; i < questions.length || i < 6;i++){
+        candidateAnswer = (input.question(questions[i]).toLowerCase());
+        candidateAnswers.push(candidateAnswer);
+        console.log('Your Answer: ' + `${candidateAnswers}`);
+        console.log('Correct Answer: ' + `${correctAnswers[i]}` + '\n);
+    }
 
 
 }
